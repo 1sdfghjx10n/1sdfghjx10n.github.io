72c5a2927405e7dc358138c08f646f56fd9a1e45
1sdfghjx10n <jxinyeo5@gmail.com>
2024-12-03 15:07:48 +0800

    tried js

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('planet-about').addEventListener('click', function() {
+    window.location.href = 'about.html';
+});
+
+// Make "planet-project" clickable and redirect to "projects.html"
+document.getElementById('planet-project').addEventListener('click', function() {
+    window.location.href = 'projects.html';
+});
