36d1b2275bba29d6badfa8ef4ded4ac9bd16d795
1sdfghjx10n <jxinyeo5@gmail.com>
2024-12-04 13:38:58 +0800

    newfolders

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function showInfo(text) {
   }
 
 console.log('script.js loaded');
+
+function createAsteroid() {
+    const asteroid = document.createElement('div');
+    asteroid.classList.add('asteroid');
+    asteroid.setAttribute('data-info', 'Asteroid: Rocky celestial object.');
+    space.appendChild(asteroid);
+  
+    asteroid.addEventListener('click', () => {
+      showInfo(asteroid.getAttribute('data-info'));
+    });
+  
+    setTimeout(() => asteroid.remove(), 5000); // Remove after 5 seconds
+  }
