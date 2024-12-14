a187bda59d8a6e97196144819cb73becfab3bf8d
1sdfghjx10n <jxinyeo5@gmail.com>
2024-12-14 12:57:22 +0800

    3JS!!!!

--- cntact/src/main.js
+++ cntact/src/main.js
@@ -1,24 +1,38 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//Create a scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
 
-setupCounter(document.querySelector('#counter'))
+//Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+//Create & add bject
+const geometry = new THREE.SphereGeometry();
+const material = new THREE.MeshNormalMaterial({ color: '#468585', wireframe: true });
+const sphere = new THREE.Mesh(geometry, material);
+
+scene.add(sphere);
+
+//Create a light
+const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+//Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Render the scene
+function animate() {
+    requestAnimationFrame(animate);
+
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+    
+    renderer.render(scene, camera);
+}
+
+animate();
